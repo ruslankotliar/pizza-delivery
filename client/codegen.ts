adfc3c91342e3a3a7756aa1854b99744c2cb4b16
ruslankotliar <ruslankot101@knu.ua>
2023-02-19 16:51:23 +0100

    Initial commit

---
+++ client/codegen.ts
@@ -0,0 +1,22 @@
+import { CodegenConfig } from '@graphql-codegen/cli';
+
+const config: CodegenConfig = {
+  schema: 'http://localhost:4000/graphql',
+  documents: ['src/**/*.tsx'],
+  generates: {
+    './src/__generated__/': {
+      preset: 'client',
+      plugins: [
+        'typescript',
+        'typescript-operations',
+        'typescript-react-apollo',
+      ],
+      presetConfig: {
+        gqlTagName: 'gql',
+      },
+    },
+  },
+  ignoreNoDocuments: true,
+};
+
+export default config;
